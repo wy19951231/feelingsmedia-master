54a01a7ebf50dec781cacfd38347e19325f36458
wy19951231 <1575456841@qq.com>
2016-07-10 09:23:03 +0800

    add html

---
+++ js/ader-reg.js
@@ -0,0 +1,21 @@
+
+
+angular.module('myApp',[])
+  .controller('FormController', function($scope){
+    // $scope.userdata = {
+    //   username: username
+    // }
+    // $scope.username="hhhh"
+
+
+    // setTimeout(function(){console.log($scope.username)},2000);
+
+
+
+    $scope.submitForm = function(){
+      // console.log('表单提交了！');
+
+      // console.log(userdata);
+    }
+  })
+
